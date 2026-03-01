b1a3d6dd43c5833cdd9aaa96f445b51cb3343744
Ariel C <acremona66@gmail.com>
2026-03-01 16:12:48 -0300

    feat(vehiculos): create Vehiculo model and register in admin

--- vehiculos/models.py
+++ vehiculos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Vehiculo(models.Model):
+
+    ESTADO_CHOICES = [
+        ('DISPONIBLE', 'Disponible'),
+        ('EN_RUTA', 'En ruta'),
+        ('MANTENIMIENTO', 'En mantenimiento'),
+        ('FUERA_DE_SERVICIO', 'Fuerda de servicio'),
+    ]
+
+    patente = models.CharField(max_length=10, unique=True)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50)
+    anio = models.PositiveIntegerField()
+    capacidad_carga_kg = models.DecimalField(max_digits=10, decimal_places=2)
+    estado = models.CharField(
+        max_length=20,
+        choices=ESTADO_CHOICES,
+        default='DISPONIBLE'
+    )
+
+    def __str__(self):
+        return f"{self.patente} - {self.marca} {self.modelo}"
